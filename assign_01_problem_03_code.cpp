3fa906356bb21ee0afd30a9d60aacdd04f14de53
mohamedosama20216678 <130155461+mohamedosama20216678@users.noreply.github.com>
2023-04-07 15:16:06 +0200

    Add files via upload

---
+++ assign_01_problem_03_code.cpp
@@ -0,0 +1,23 @@
+//Mohamed Osama
+//202106678
+#include <iostream>
+using namespace std;
+int main()
+{
+    int S;
+    long long A;
+    char X;
+    float F;
+    double Y;
+    cin>>S;
+    cin>>A;
+    cin>>X;
+    cin>>F;
+    cin>>Y;
+       cout<<S<<endl;
+       cout<<A<<endl;
+       cout<<X<<endl;
+       cout<<F<<endl;
+       cout<<Y<<endl;
+    return 0;
+}
---
+++ assign_01_problem_06_code.cpp
@@ -0,0 +1,19 @@
+//Mohamed Osama
+//202106678
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int x;
+    cin >> x;
+ 
+    int first_digit = x / 1000;
+    if (first_digit % 2 == 0) {
+        cout << "EVEN" << endl;
+    } else {
+        cout << "ODD" << endl;
+    }
+ 
+    return 0;
+}
